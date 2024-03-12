a32f023666163144f7f49c2c389c7d8fd13c5fca
Allen Lee <6207ALS@gmail.com>
2024-03-12 17:40:53 -0400

    Installed 'github-app-installer' in 'src' directory, moved its 'public' folder to root of project

--- src/commands/setup.ts
+++ src/commands/setup.ts
@@ -1,7 +1,7 @@
 /* eslint-disable */
 
 import {Command} from '@oclif/core'
-// import main from "../github-app-installer/installer.js";
+import main from "../github-app-installer/installer.js";
 
 export default class Setup extends Command {
   static description = 'describe the command here'
@@ -22,6 +22,6 @@ export default class Setup extends Command {
   // }
 
   public async run(): Promise<void> {
-    // await main();
+    await main();
   }
 }
